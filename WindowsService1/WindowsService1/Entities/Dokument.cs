8c0943cd30f8efa3b491cced5f9c98021f4d942a
Darius Cebrat <dcmieczyslaw@gmail.com>
2022-08-19 08:15:23 +0200

    adding uslugi

---
+++ WindowsService1/WindowsService1/Entities/Dokument.cs
@@ -0,0 +1,24 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace WindowsService1.Entities
+//{
+//    public class Dokument
+//    {
+//        public string NazwaKontrahenta { get; set; }
+//        public string Email { get; set; }
+//        public string NumerDokumentu { get; set; }
+//        public string Data { get; set; }
+//        public  string[] Pozycje { get; set; }
+
+//        public override string ToString()
+//        {
+//            return $"dokument:{NumerDokumentu}, kontrahent{NazwaKontrahenta}, email:{Email}";
+//        }
+//    }
+
+    
+//}
---
+++ WindowsService1/WindowsService1/Services/FileWriteService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WindowsService1.Services
+{
+    public interface IFileWriteService
+    {
+        void CreateFileWyslane(string fileNameWithPath);
+        void CreateFileNieWyslane(string fileNameWithPath, string errorMessage,string stackTrace);
+    }
+    public class FileWriteService: IFileWriteService
+    {
+        
+
+        private  void EnsureDirectoryExists(string filePath)
+        {
+            FileInfo fi = new FileInfo(filePath);
+            if (!fi.Directory.Exists)
+            {
+                System.IO.Directory.CreateDirectory(fi.DirectoryName);
+            }
+        }
+
+
+        //do przetestowania
+        public void CreateFileWyslane(string fileNameWithPath)
+        {
+            string savePath = @"C:\Users\48502\Desktop\Wyslane\";
+            string fileName = fileNameWithPath.Remove(0, fileNameWithPath.LastIndexOf('\\') + 1);
+            string wholeSavePath = savePath + fileName;
+            try
+            {
+                EnsureDirectoryExists(savePath);
+                File.Move(fileNameWithPath, wholeSavePath);
+            }
+            catch (Exception e)
+            {
+
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                    eventLog.WriteEntry("Błąd przenoszeniu plikow " + e.Message +"\n\n"+ e.StackTrace, EventLogEntryType.Error);
+                };
+            }
+           
+
+        }
+        public void CreateFileNieWyslane(string fileNameWithPath, string errorMessage, string stackTrace)
+        {
+            string savePath = @"C:\Users\48502\Desktop\NieWyslane\";
+            string fileName = fileNameWithPath.Remove(0, fileNameWithPath.LastIndexOf('\\')+1);
+            string wholeSavePath = savePath + fileName;
+            try
+            {
+                EnsureDirectoryExists(savePath);
+                File.Move(fileNameWithPath, wholeSavePath);
+                using (StreamWriter writer = new StreamWriter(wholeSavePath, true))
+                {
+                    writer.WriteLine(errorMessage);
+                    writer.WriteLine(stackTrace);
+
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                using (EventLog eventLog = new EventLog("Application"))
+                {
+                    eventLog.Source = "Application";
+                     eventLog.WriteEntry("Błąd : " + e.Message + "\n\n" + e.StackTrace, EventLogEntryType.Error);
+                };
+            }
+           
+        }
+    }
+}
